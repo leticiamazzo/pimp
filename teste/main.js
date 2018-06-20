a3e54b9c6f9808ff1e222db4a504c11aa47021a7
Leticia Mazzo <leticiamazzo@gmail.com>
2018-06-20 17:43:12 -0300

    teste iniciado

--- teste/main.js
+++ teste/main.js
@@ -1,105 +1,101 @@
-/* Objeto com as perguntas */
-let questions = [
-  {
-    text: "Você mora em casa ou apartamento?",
-    answer: true
-  },
-  {
-    text: "Você procura reduzir o consumo de que tipo de recurso?",
-    answer: false
-  },
-  {
-    text: "Quanto você pode investir?",
-    answer: true  
-  }
-]
+let resp1
+let resp2
+let resp3
 
-/* Elementos necessarios para o quiz funcionar */
-const questionsAll = document.getElementById("questions")
-const closeButton = document.getElementById("close")
-const startButton = document.querySelector("#container button")
-const overlay = document.getElementById("overlay")
-const timer = document.getElementById("timer")
-const container = document.getElementById("container")
+let respClicada1
+let respClicada2
+let respClicada3
 
-/* Abrir e fechar o quiz */
-let interval = undefined
-startButton.addEventListener("click", openQuiz)
-closeButton.addEventListener("click", closeQuiz)
+let concatResps
 
-function openQuiz() {
-  overlay.style.top = "0vh"
-  interval = startTimer(timer)
-}
+// let concat
+// function chamaResultado(resp1, resp2, resp3){
+//   respFinal = resp1 + resp2 + resp3
 
-function closeQuiz() {
-  setTimeout(function () {
-    questionsAll.style.left = "0vw"
-  }, 500)
-  overlay.style.top = "100vh"
-  clearInterval(interval)
-}
+// }
 
-/* Crio o HTML das perguntas */
-for (let i = 0; i < questions.length; i++) {
-  const questionDiv = document.createElement("div")
-  questionDiv.classList.add("question")
-  questionDiv.style.left = 100 * i + "vw"
+function clicaImovel(valor) {
+  resp1 = valor
+  resp2 = valor
+  resp3 = valor
 
-  const questionP = document.createElement("p")
-  questionP.textContent = questions[i].text
-
-  const answersDiv = document.createElement("div")
-  answersDiv.classList.add("answers")
-
-  const trueButton = document.createElement("button")
-  trueButton.textContent = "VERDADEIRO"
-  const falseButton = document.createElement("button")
-  falseButton.textContent = "FALSO"
-
-  questionDiv.appendChild(questionP)
-
-  answersDiv.appendChild(trueButton)
-  answersDiv.appendChild(falseButton)
-
-  questionDiv.appendChild(answersDiv)
-
-  questionsAll.appendChild(questionDiv)
-
-  trueButton.addEventListener("click", function() {
-    questions[i].userAnswer = true
-    nextQuizStep(i)
-  })
-
-  falseButton.addEventListener("click", function() {
-    questions[i].userAnswer = false
-    nextQuizStep(i)
-  })
-}
-
-function nextQuizStep(index) {
-  if (index < questions.length - 1) {
-    questionsAll.style.left = `calc(${questionsAll.offsetLeft}px - 100vw)`
+  if (resp1 === "a") {
+    respClicada1 = resp1
   } else {
-    let answeredQuestions = ''
-    for (const question of questions) {
-      answeredQuestions += `
-        <li>
-          ${question.text}<br />
-          <span class="${question.userAnswer === question.answer ? "right" : "wrong"}">
-            ${question.userAnswer === true ? "VERDADEIRO" : "FALSO"}
-          </span>
-        </li>
-      `
-    }
+    respClicada1 = resp1
+  }
+  console.log(respClicada1)
+}
 
-    container.innerHTML = `<ul>${answeredQuestions}</ul>`
+function clicaImovel2(valor) {
+  resp2 = valor
 
-    const tryAgainButton = document.createElement("button")
-    tryAgainButton.textContent = "Tente novamente"
-    tryAgainButton.addEventListener("click", openQuiz)
-    container.appendChild(tryAgainButton)
+  if (resp2 === "a") {
+    respClicada2 = resp2
+  } else {
+    respClicada2 = resp2
+  }
+  console.log(respClicada2)
+}
 
-    closeQuiz()
+function clicaImovel3(valor) {
+  resp3 = valor
+
+  if (resp3 === "a") {
+    respClicada3 = resp3
+  } else {
+    respClicada3 = resp3
+  }
+  console.log(respClicada3)
+}
+
+function armazenaResultado() {
+  concatResps = respClicada1 + respClicada2 + respClicada3
+  console.log(concatResps)
+
+  if(concatResps === "aaa") {
+    document.getElementById("resultado").innerHTML =
+    `<p>Colete água da chuva utilizando grandes recipientes. Utilize para regar o jardim, lavar o quintal ou usar na descarga</p>
+    <p>Para áreas externas opte por pisos permeáveis</p>
+    <p>Reutilize materiais e consuma menos. Reaproveite!
+    </p>
+    <p>Opte por um vaso sanitário com caixa acoplada e acionamento duplo, pois resíduos líquidos não precisam de uma descarga completa</p>`
+  } else if (concatResps === "aab") {
+    document.getElementById("resultado").innerHTML =
+    `<p>Colete água da chuva utilizando grandes recipientes. Utilize para regar o jardim, lavar o quintal ou usar na descarga</p>
+    <p>Para áreas externas opte por pisos permeáveis</p>
+    <p>Reutilize materiais e consuma menos. Reaproveite!
+    </p>
+    <p>Opte por um vaso sanitário com caixa acoplada e acionamento duplo, pois resíduos líquidos não precisam de uma descarga completa</p>`
+  } else if (concatResps === "aba") {
+    document.getElementById("resultado").innerHTML =
+    `<p>Ambientes integrados (sem divisão por paredes) e com janelas, permitem maior circulação de ar e proporcionam melhor iluminação natural, minimizando o uso de sistema de ar-condicionado e iluminação artificial.</p>
+    <p>Para iluminação artificial existem lâmpadas de baixo consumo, como as fluorescentes ou de LED</p>
+    <p>Os panos de vidro (como janelas, porém sem abertura) também funcionam para trazer mais luz natural ao ambiente. Para espaços centrais, onde não é possível realizar aberturas nas paredes, clarabóias (aberturas no alto da construção “cobertas” com material translúcido que permite entrada de luz ou até ventilação) podem ser uma boa opção</p>
+    <p>Invista em captação de energia solar. A princípio o investimento é alto, mas torna-se vantajoso a longo prazo</p>`
+  } else if (concatResps === "abb"){
+    document.getElementById("resultado").innerHTML =
+    `<p>Ambientes integrados (sem divisão por paredes) e com janelas, permitem maior circulação de ar e proporcionam melhor iluminação natural, minimizando o uso de sistema de ar-condicionado e iluminação artificial.</p>
+    <p>Para iluminação artificial existem lâmpadas de baixo consumo, como as fluorescentes ou de LED</p>
+    <p>Os panos de vidro (como janelas, porém sem abertura) também funcionam para trazer mais luz natural ao ambiente. Para espaços centrais, onde não é possível realizar aberturas nas paredes, clarabóias (aberturas no alto da construção “cobertas” com material translúcido que permite entrada de luz ou até ventilação) podem ser uma boa opção</p>
+    <p>Invista em captação de energia solar. A princípio o investimento é alto, mas torna-se vantajoso a longo prazo</p>`
+  } else if (concatResps === "bbb") {
+    document.getElementById("resultado").innerHTML =
+    `<p>Ambientes integrados (sem divisão por paredes) e com janelas, permitem maior circulação de ar e proporcionam melhor iluminação natural, minimizando o uso de sistema de ar-condicionado e iluminação artificial</p>
+    <p>Para a iluminação artificial, existem as lâmpadas de baixo consumo de energia elétrica, como as fluorescentes</p>`
+  } else if (concatResps === "bba") {
+    document.getElementById("resultado").innerHTML =
+    `<p>Ambientes integrados (sem divisão por paredes) e com janelas, permitem maior circulação de ar e proporcionam melhor iluminação natural, minimizando o uso de sistema de ar-condicionado e iluminação artificial</p>
+    <p>Para a iluminação artificial, existem as lâmpadas de baixo consumo de energia elétrica, como as fluorescentes</p>`
+  } else if (concatResps === "bab"){
+    document.getElementById("resultado").innerHTML =
+    `<p>Utilize a água da máquina de lavar, por exemplo, na descarga</p>
+    <p>Reutilize materiais e consuma menos. Reaproveite!</p>
+    <p>Opte por um vaso com caixa acoplada e acionamento duplo, pois resíduos líquidos não precisam de uma descarga completa</p>`
+  } else {
+    document.getElementById("resultado").innerHTML =
+    `<p>Utilize a água da máquina de lavar, por exemplo, na descarga</p>
+    <p>Reutilize materiais e consuma menos. Reaproveite!</p>
+    <p>Opte por um vaso com caixa acoplada e acionamento duplo, pois resíduos líquidos não precisam de uma descarga completa</p>`
   }
 }
